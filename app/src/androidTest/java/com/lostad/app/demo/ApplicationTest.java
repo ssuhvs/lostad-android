ebd8da2492719bc8fd51b604690fe58c741d56b4
ssuhvs <ssz342905>
2016-01-11 09:26:26 +0800

    WaterDropListView 优化

--- app/src/androidTest/java/com/lostad/app/demo/ApplicationTest.java
+++ app/src/androidTest/java/com/lostad/app/demo/ApplicationTest.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 /**
+ * ssz test
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
