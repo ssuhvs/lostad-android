9fd47beab4fb34ac79ac0018e61d615bdf43dec3
ssuhvs <ssz342905>
2016-01-29 14:56:09 +0800

    修改ToolBar菜单字体

--- lib_lostad/src/main/java/com/lostad/applib/util/ImageChooser.java
+++ lib_lostad/src/main/java/com/lostad/applib/util/ImageChooser.java
@@ -15,28 +15,27 @@ import android.provider.MediaStore;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
 /**
  * 窗口工具类,提供可重用的窗口
  * @author sszvip
- *
  */
 public class ImageChooser {
-	
-    private static int mSelectedIndex = -1;
-	public static final int REQUEST_CODE_CROP    = -10001;//裁剪
-    public static final String FILE_NAME_PIC_TEMP    = "temp_pic";
-    private static final int SCALE = 5;//照片缩小比例
-    private static int width =320,height=320;
-    
-    public static void setWidth(int w){
-    	width = w;
-    }
-    public static void setHeight(int h){
-    	height = h;
-    }
-    
-	public static void showImagePickerForResult(final Activity context,final int requestCode){
+
+	public static final int REQUEST_CODE_CROP     = 65535;//裁剪
+	public static final int REQUEST_CODE_PIC      = 65534;//图片
+	public static final int REQUEST_CODE_PHOTO    = 65533;//相机
+	public static final String FILE_NAME_PIC_TEMP    = "temp_pic";
+	private static final int SCALE = 5;//照片缩小比例
+	private static int width =320,height=320;
+
+	public static void setWidth(int w){
+		width = w;
+	}
+	public static void setHeight(int h){
+		height = h;
+	}
+
+	public static void showImagePickerForResult(final Activity context){
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		builder.setTitle("图片来源");
 		builder.setNegativeButton("取消", null);
@@ -45,37 +44,35 @@ public class ImageChooser {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				switch (which) {
-				case 0:
-					mSelectedIndex = 0;
-					Uri imageUri = null;
-					Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-					ImageUtil.deleteFile(Environment.getExternalStorageDirectory().getAbsolutePath(), FILE_NAME_PIC_TEMP);
-					imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory().getAbsolutePath(),FILE_NAME_PIC_TEMP));
-					//指定照片保存路径（SD卡），image.jpg为一个临时文件，每次拍照后这个图片都会被替换
-					openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-					context.startActivityForResult(openCameraIntent, requestCode);
-					break;
-				case 1:
-					mSelectedIndex = 1;
-					Intent intent = new Intent();  
-			        intent.setType("image/*");  
-			        intent.setAction(Intent.ACTION_PICK);  
-			        context.startActivityForResult(intent, requestCode);
-					break;
+					case 0:
+						Uri imageUri = null;
+						Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+						ImageUtil.deleteFile(Environment.getExternalStorageDirectory().getAbsolutePath(),FILE_NAME_PIC_TEMP);
+						imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory().getAbsolutePath(),FILE_NAME_PIC_TEMP));
+						//指定照片保存路径（SD卡），image.jpg为一个临时文件，每次拍照后这个图片都会被替换
+						openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+						context.startActivityForResult(openCameraIntent, REQUEST_CODE_PHOTO);
+						break;
+					case 1:
+						Intent intent = new Intent();
+						intent.setType("image/*");
+						intent.setAction(Intent.ACTION_PICK);
+						context.startActivityForResult(intent, REQUEST_CODE_PIC);
+						break;
 
-				default:
-					break;
+					default:
+						break;
 				}
 			}
 		});
 		builder.create().show();
 	}
-	
-	
-	public static void onActivityResult(Activity ctx,boolean isCrop, Intent data,ImageCallback callback) {
 
-			switch (mSelectedIndex) {//上次选择的索引
-			case 0://相机
+
+	public static void onActivityResult(Activity ctx,int requestCode,boolean isCrop, Intent data,ImageCallback callback) {
+
+		switch (requestCode) {//上次选择的索引
+			case REQUEST_CODE_PHOTO://相机
 				if(isCrop){//裁剪
 					Uri uri = null;
 					if (data != null) {
@@ -92,11 +89,11 @@ public class ImageChooser {
 					bitmap.recycle();
 					callback.onPicSelected(newBitmap);
 				}
-				
+
 				break;
 
-			case 1: //选择的照片的数据
-				
+			case REQUEST_CODE_PIC: //选择的照片的数据
+
 				if(isCrop){//裁剪
 
 					Uri uri = null;
@@ -109,14 +106,14 @@ public class ImageChooser {
 					}
 					cropImage(ctx,uri,width, height, REQUEST_CODE_CROP);
 					break;
-					
+
 				}else{
-					
+
 					ContentResolver resolver = ctx.getContentResolver();
 					//照片的原始资源地址
-					Uri originalUri = data.getData(); 
-		            try {
-		            	//使用ContentProvider通过URI获取原始图片
+					Uri originalUri = data.getData();
+					try {
+						//使用ContentProvider通过URI获取原始图片
 						Bitmap photo = MediaStore.Images.Media.getBitmap(resolver, originalUri);
 						if (photo != null) {
 							//为防止原始图片过大导致内存溢出，这里先缩小原图显示，然后释放原始Bitmap占用的内存
@@ -126,14 +123,14 @@ public class ImageChooser {
 							callback.onPicSelected(smallBitmap);
 						}
 					} catch (FileNotFoundException e) {
-					    e.printStackTrace();
+						e.printStackTrace();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-		            
+
 				}
 				break;
-				
+
 			case REQUEST_CODE_CROP://裁剪结果
 				Bitmap photo = null;
 				Uri photoUri = data.getData();
@@ -143,19 +140,19 @@ public class ImageChooser {
 				if (photo == null) {
 					Bundle extra = data.getExtras();
 					if (extra != null) {
-		                photo = (Bitmap)extra.get("data");  
-		            }  
+						photo = (Bitmap)extra.get("data");
+					}
 				}
 				callback.onPicSelected(photo);
-				
+
 				break;
-			}
+		}
 	}
-	
-	
-	
+
+
+
 	//截取图片
-		public static void cropImage(Activity ctx,Uri uri, int outputX, int outputY, int requestCode){
+	private static void cropImage(Activity ctx,Uri uri, int outputX, int outputY, int requestCode){
 //			Intent intent = new Intent("com.android.camera.action.CROP");  
 //	        intent.setDataAndType(uri, "image/*");  
 //	        intent.putExtra("crop", "true");  
@@ -167,23 +164,23 @@ public class ImageChooser {
 //	        intent.putExtra("noFaceDetection", true);
 //	        intent.putExtra("return-data", true);  
 //	        ctx.startActivityForResult(intent, requestCode);
-			
-			Intent intent = new Intent("com.android.camera.action.CROP");
-			intent.setDataAndType(uri, "image/*");
-			// 设置裁剪
-			intent.putExtra("crop", "true");
-			// aspectX aspectY 是宽高的比例
-			intent.putExtra("aspectX", 1);
-			intent.putExtra("aspectY", 1);
-			// outputX outputY 是裁剪图片宽高
-			intent.putExtra("outputX", outputX);
-			intent.putExtra("outputY", outputY);
-			intent.putExtra("return-data", true);
-			ctx.startActivityForResult(intent, requestCode);
-		}
-		
-		
-		public interface ImageCallback{
-			void onPicSelected(Bitmap bitmap);
-		}
+
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(uri, "image/*");
+		// 设置裁剪
+		intent.putExtra("crop", "true");
+		// aspectX aspectY 是宽高的比例
+		intent.putExtra("aspectX", 1);
+		intent.putExtra("aspectY", 1);
+		// outputX outputY 是裁剪图片宽高
+		intent.putExtra("outputX", outputX);
+		intent.putExtra("outputY", outputY);
+		intent.putExtra("return-data", true);
+		ctx.startActivityForResult(intent, requestCode);
+	}
+
+
+	public interface ImageCallback{
+		void onPicSelected(Bitmap bitmap);
+	}
 }
